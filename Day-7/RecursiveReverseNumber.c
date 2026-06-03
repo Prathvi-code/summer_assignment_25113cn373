e702dc15e68ec4ce9f8c9236bf8f852e73834ca2
Prathvi <prathvi123006@gmail.com>
2026-06-03 18:08:50 +0530

    first commit

---
+++ Day-7/RecursiveReverseNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int reverse(int n, int rev) {
+    if (n == 0)   
+        return rev;
+    else
+        return reverse(n / 10, rev * 10 + (n % 10));   
+}
+
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    printf("Reverse = %d\n", reverse(n, 0));
+    return 0;
+}
